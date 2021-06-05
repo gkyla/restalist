f2bd66245ac4df7c298511a2ec4bafda4016d6e4
gkyla <agitsyang@gmail.com>
2021-06-06 01:18:28 +0700

    🚫 remove DOM codes

--- src/scripts/data/restaurant-data.js
+++ src/scripts/data/restaurant-data.js
@@ -6,48 +6,44 @@ import dataRes from './DATA.json';
 // Data yang saya tambahkan dari Submission 1
 
 class restalistSourceData {
-    static async restaurantFilterBestWeek() {
-        const gridWeek = document.querySelector('.grid-week');
-        const response = await fetch(API_ENDPOINT.LIST);
-        const data = await response.json();
-        gridWeek.innerHTML = '';
-        return data.restaurants;
-    }
+  static async restaurantFilterBestWeek() {
+    const response = await fetch(API_ENDPOINT.LIST);
+    const data = await response.json();
+    return data.restaurants;
+  }
 
-    static restaurantCafe() {
-        const dataNewRestaurants = dataRes.newRestaurant;
-        return dataNewRestaurants;
-    }
+  static restaurantCafe() {
+    const dataNewRestaurants = dataRes.newRestaurant;
+    return dataNewRestaurants;
+  }
 
-    static async restaurantList() {
-        const gridList = document.querySelector('.grid-list');
-        const response = await fetch(API_ENDPOINT.LIST);
-        const data = await response.json();
-        gridList.innerHTML = '';
-        return data.restaurants;
-    }
+  static async restaurantList() {
+    const response = await fetch(API_ENDPOINT.LIST);
+    const data = await response.json();
+    return data.restaurants;
+  }
 
-    static async detailRestaurantList(id) {
-        const loading = document.querySelector('.loading');
-        const response = await fetch(API_ENDPOINT.DETAIL(id));
-        const data = await response.json();
-        loading.style.display = 'none';
-        return data.restaurant;
-    }
+  static async detailRestaurantList(id) {
+    const loading = document.querySelector('.loading');
+    const response = await fetch(API_ENDPOINT.DETAIL(id));
+    const data = await response.json();
+    loading.style.display = 'none';
+    return data.restaurant;
+  }
 
-    static async detailRestaurantComent(dataComment) {
-        const response = await fetch(API_ENDPOINT.REVIEW, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-Auth-Token': CONFIG.API_KEY,
-            },
-            body: JSON.stringify(dataComment),
-        });
-        const data = await response.json();
-        const allData = RefreshComment(data);
-        return allData;
-    }
+  static async detailRestaurantComent(dataComment) {
+    const response = await fetch(API_ENDPOINT.REVIEW, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Auth-Token': CONFIG.API_KEY,
+      },
+      body: JSON.stringify(dataComment),
+    });
+    const data = await response.json();
+    const allData = RefreshComment(data);
+    return allData;
+  }
 }
 
 export default restalistSourceData;
