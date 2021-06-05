33628cce01c4666bc43030649a87d48fe4ed6f1f
Agits Zakky Akmala <67510503+gkyla@users.noreply.github.com>
2021-06-05 22:58:35 +0700

    change API base url

--- src/scripts/global-setting/config.js
+++ src/scripts/global-setting/config.js
@@ -1,8 +1,8 @@
 const CONFIG = {
     API_KEY: '12345',
-    BASE_URL_API: 'https://dicoding-restaurant-api.el.r.appspot.com',
-    BASE_URL_API_IMAGE_MEDIUM: 'https://dicoding-restaurant-api.el.r.appspot.com/images/medium',
-    BASE_URL_API_IMAGE_SMALL: 'https://dicoding-restaurant-api.el.r.appspot.com/images/small',
+    BASE_URL_API: 'https://restaurant-api.dicoding.dev',
+    BASE_URL_API_IMAGE_MEDIUM: 'https://restaurant-api.dicoding.dev/images/medium',
+    BASE_URL_API_IMAGE_SMALL: 'https://restaurant-api.dicoding.dev/images/small',
     DATABASE_NAME: 'restalist-db',
     OBJECT_STORE_NAME: 'restaurant',
     DATABASE_VERSION: 1,
