a6e798a440d6fb9ea0c50807b6e68313a0a315a0
gkyla <agitsyang@gmail.com>
2021-06-06 01:19:42 +0700

    🚀 change bundleAnalyzer config

--- webpack.common.js
+++ webpack.common.js
@@ -13,139 +13,142 @@ const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
-   entry: path.resolve(__dirname, 'src/scripts/index.js'),
-   output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: '[name].bundle.js',
-   },
-   optimization: {
-      splitChunks: {
-         chunks: 'all',
-         minSize: 20000,
-         maxSize: 160000,
-         minChunks: 1,
-         maxAsyncRequests: 30,
-         maxInitialRequests: 30,
-         automaticNameDelimiter: '~',
-         enforceSizeThreshold: 50000,
-         cacheGroups: {
-            defaultVendors: {
-               test: /[\\/]node_modules[\\/]/,
-               priority: -10,
-            },
-            default: {
-               minChunks: 2,
-               priority: -20,
-               reuseExistingChunk: true,
-            },
-         },
+  entry: path.resolve(__dirname, 'src/scripts/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minSize: 20000,
+      maxSize: 160000,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      automaticNameDelimiter: '~',
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
       },
-   },
-   module: {
-      rules: [
-         {
-            test: /\.css$/,
-            use: [
-               {
-                  loader: 'style-loader',
-               },
-               {
-                  loader: 'css-loader',
-               },
-            ],
-         },
-         {
-            test: /\.(png|jpg|webp)$/,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|webp)$/,
+        loader: 'file-loader',
+        include: path.join(__dirname, 'src'),
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
             loader: 'file-loader',
-            include: path.join(__dirname, 'src'),
-         },
-         {
-            test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-            use: [
-               {
-                  loader: 'file-loader',
-                  options: {
-                     name: '[name].[ext]',
-                     outputPath: 'font/',
-                  },
-               },
-            ],
-         },
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'font/',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/templates/index.html'),
+      filename: 'index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/public/'),
+          to: path.resolve(__dirname, 'dist/'),
+          globOptions: {
+            ignore: ['**/images/heros/**'],
+          },
+        },
       ],
-   },
-   plugins: [
-      new CleanWebpackPlugin(),
-      new HtmlWebpackPlugin({
-         template: path.resolve(__dirname, 'src/templates/index.html'),
-         filename: 'index.html',
-      }),
-      new CopyWebpackPlugin({
-         patterns: [
-            {
-               from: path.resolve(__dirname, 'src/public/'),
-               to: path.resolve(__dirname, 'dist/'),
-               globOptions: {
-                  ignore: ['**/images/heros/**'],
-               },
-            },
-         ],
-      }),
-      new WorkboxWebpackPlugin.InjectManifest({
-         swSrc: './src/scripts/sw.js',
-         swDest: 'sw.js',
-      }),
-      new WebpackPwaManifest({
-         filename: 'manifest.json',
-         name: 'Restalist',
-         short_name: 'Restalist',
-         start_url: '/index.html',
-         display: 'standalone',
-         ios: true,
-         inject: true,
-         fingerprints: false,
-         description: 'Looking for a restaurant is now easier than you imagine',
-         background_color: '#ececec',
-         crossorigin: 'use-credentials',
-         theme_color: '#ececec',
-         icons: [
-            {
-               src: path.resolve('src/public/images/logo.png'),
-               sizes: [96, 128, 192, 256, 384, 512],
-               destination: path.join('icons'),
-            },
-            {
-               src: path.resolve('src/public/images/maskable_icon.png'),
-               size: '640x640',
-               purpose: 'maskable',
-            },
-         ],
-      }),
-      new ImageminWebpWebpackPlugin({
-         config: [
-            {
-               test: /\.(jpe?g|png)/,
-               options: {
-                  quality: 50,
-               },
-            },
-         ],
-         overrideExtension: true,
-      }),
-      new ImageminWebpackPlugin({
-         plugins: [
-            imageminOptipng({
-               optimizationLevel: 7,
-            }),
-            ImageminMozjpeg({
-               quality: 50,
-               progressive: true,
-            }),
-            imageminWebp({
-               quality: 50,
-            }),
-         ],
-      }),
-      new CompressionPlugin(),
-      new BundleAnalyzerPlugin(),
-   ],
+    }),
+    new WorkboxWebpackPlugin.InjectManifest({
+      swSrc: './src/scripts/sw.js',
+      swDest: 'sw.js',
+    }),
+    new WebpackPwaManifest({
+      filename: 'manifest.json',
+      name: 'Restalist',
+      short_name: 'Restalist',
+      start_url: '/index.html',
+      display: 'standalone',
+      ios: true,
+      inject: true,
+      fingerprints: false,
+      description: 'Looking for a restaurant is now easier than you imagine',
+      background_color: '#ececec',
+      crossorigin: 'use-credentials',
+      theme_color: '#ececec',
+      icons: [
+        {
+          src: path.resolve('src/public/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('icons'),
+        },
+        {
+          src: path.resolve('src/public/images/maskable_icon.png'),
+          size: '640x640',
+          purpose: 'maskable',
+        },
+      ],
+    }),
+    new ImageminWebpWebpackPlugin({
+      config: [
+        {
+          test: /\.(jpe?g|png)/,
+          options: {
+            quality: 50,
+          },
+        },
+      ],
+      overrideExtension: true,
+    }),
+    new ImageminWebpackPlugin({
+      plugins: [
+        imageminOptipng({
+          optimizationLevel: 7,
+        }),
+        ImageminMozjpeg({
+          quality: 50,
+          progressive: true,
+        }),
+        imageminWebp({
+          quality: 50,
+        }),
+      ],
+    }),
+    new CompressionPlugin(),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+    }),
+  ],
 };
